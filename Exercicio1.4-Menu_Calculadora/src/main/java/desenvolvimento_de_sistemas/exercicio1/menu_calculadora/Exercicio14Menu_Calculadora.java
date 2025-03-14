663df05472b035ec06dda8a2b1c66999db34b77e
José Vicente <160607761+JoseVicente-Carv@users.noreply.github.com>
2025-03-14 23:13:30 +0000

    Terminei o Exercício 1.4 - Menu de Calculadora.

--- Exercicio1.4-Menu_Calculadora/src/main/java/desenvolvimento_de_sistemas/exercicio1/menu_calculadora/Exercicio14Menu_Calculadora.java
+++ Exercicio1.4-Menu_Calculadora/src/main/java/desenvolvimento_de_sistemas/exercicio1/menu_calculadora/Exercicio14Menu_Calculadora.java
@@ -24,6 +24,8 @@ public class Exercicio14Menu_Calculadora {
         Boolean menu = true;
         int primeiroNumero;
         int segundoNumero;
+        float resultado;
+        String operacao;
         
 //        inicar laço de retição infinito condicional.
         while (menu) {
@@ -32,9 +34,42 @@ public class Exercicio14Menu_Calculadora {
             System.out.println("+. Adição; \n-. Subtração; \n*. Multiplicação; \n/. Divisão;");
             System.out.println("s. Sair.");
             
+            // usuario escolhe os dois numeros componentes da equação
             System.out.print("Digite o primeiro número da operação: ");
             primeiroNumero = painel.nextInt();
-        }
+
+            System.out.print("Digite o segundo número da operação: ");
+            segundoNumero = painel.nextInt();
+
+            // usuario escolhe o tipo de operação
+            operacao = painel.next();
+
+            switch (operacao) {
+                case "+":
+                    resultado = (primeiroNumero + segundoNumero);
+                    break;
+
+                case "-":
+                    resultado = (primeiroNumero - segundoNumero);
+                    break;
+
+                case "*":
+                    resultado = (primeiroNumero * segundoNumero);
+                    break;
+
+                case "/":
+                    resultado = (primeiroNumero / segundoNumero);
+                    break;
+
+                case "s":
+                    menu = false;
+                    break;
+            
+                default:
+                System.out.println("Operação inválida. \nTente novamente.");
+                    break;
+            } // fim do switch-case
+        } // fim do laço de retição infinito WHILE
         
         painel.close();
         System.out.println("\n\tTarefa concluída!");
